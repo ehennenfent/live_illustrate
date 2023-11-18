7017337b7a6f39638d6aea22d02aa0ab48bc884c
Eric Hennenfent <ecapstone@gmail.com>
2023-11-18 15:55:59 -0800

    Wrap printing in try/except

--- transcribe.py
+++ transcribe.py
@@ -9,6 +9,9 @@ from time import sleep
 from threading import Thread
 
 import speech_recognition as sr
+from openai import OpenAI
+
+SYSTEM_PROMPT = "You are a helpful assistant that describes scenes to people who cannot see them. You will be given several lines of dialogue that contain details about the physical surroundings of the characters. Your job is to describe the details of the scene in a way that makes sense to someone not present. Remember to be concise and descriptive."
 
 
 def get_args():
@@ -52,12 +55,17 @@ def transcription_thread(recorder, model, sample_rate, sample_width):
                     sr.AudioData(audio_queue.get(), sample_rate, sample_width), model=model
                 ).strip()
                 text_queue.put(text)
-                print(text, file=outf, flush=True)
+                try:
+                    print(text, file=outf, flush=True)
+                except Exception as e:
+                    print("failed to write text to file:", e)
             sleep(0.25)
 
 
 def main():
     args = get_args()
+
+    # openai_client = OpenAI()
 
     # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     recorder = sr.Recognizer()
@@ -77,7 +85,20 @@ def main():
     while True:
         try:
             while not text_queue.empty():
-                print(text_queue.get())
+                text = text_queue.get()
+                print(text)
+            #     text_buffer.append(text)
+
+            # if len(text_buffer) % 10 == 0:
+            #     response = openai_client.chat.completions.create(
+            #         model="gpt-3.5-turbo",
+            #         messages=[
+            #             {"role": "system", "content": SYSTEM_PROMPT},
+            #             {"role": "user", "content": "\n".join(text_buffer[-10:])},
+            #         ]
+            #     )
+            #     print(response)
+
             sleep(0.25)
         except KeyboardInterrupt:
             print("Bye")
