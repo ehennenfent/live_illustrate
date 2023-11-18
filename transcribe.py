60fa85827810ff14c6da9019fe347216fe272617
Eric Hennenfent <ecapstone@gmail.com>
2023-11-18 01:51:15 -0800

    Mostly fix live transcription

--- transcribe.py
+++ transcribe.py
@@ -3,127 +3,67 @@
 # From https://github.com/davabase/whisper_real_time/blob/master/transcribe_demo.py
 
 import argparse
-import io
-import os
-import speech_recognition as sr
-import whisper
-
-from datetime import datetime, timedelta
+from datetime import datetime
 from queue import Queue
-from tempfile import NamedTemporaryFile
 from time import sleep
-from sys import platform
+
+import speech_recognition as sr
+
+data_queue = Queue()
+
+
+def record_callback(_, audio) -> None:
+    data_queue.put(audio.get_raw_data())
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="medium.en", help="Model to use",
-                        choices=["tiny.en", "base.en", "small.en", "medium.en", "large", "large-v2", "large-v3"])
-    parser.add_argument("--energy_threshold", default=1000,
-                        help="Energy level for mic to detect.", type=int)
-    parser.add_argument("--record_timeout", default=2,
-                        help="How real time the recording is in seconds.", type=float)
-    parser.add_argument("--phrase_timeout", default=3,
-                        help="How much empty space between recordings before we "
-                             "consider it a new line in the transcription.", type=float)
+    parser.add_argument(
+        "--model",
+        default="medium.en",
+        help="Model to use",
+        choices=[
+            "tiny.en",
+            "base.en",
+            "small.en",
+            "medium.en",
+            "large",
+            "large-v2",
+            "large-v3",
+        ],
+    )
+    parser.add_argument(
+        "--dynamic_energy_threshold",
+        action="store_true",
+        default=True,
+        help="Adjust the energy threshold dynamically",
+    )
     args = parser.parse_args()
 
-    # The last time a recording was retrieved from the queue.
-    phrase_time = None
-    # Current raw audio bytes.
-    last_sample = bytes()
-    # Thread safe Queue for passing data from the threaded recording callback.
-    data_queue = Queue()
     # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     recorder = sr.Recognizer()
-    recorder.energy_threshold = args.energy_threshold
-    # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
-    recorder.dynamic_energy_threshold = False
-
+    recorder.dynamic_energy_threshold = args.dynamic_energy_threshold
     source = sr.Microphone(sample_rate=16000)
-
-    # Load / Download model
-    model = args.model
-    audio_model = whisper.load_model(model)
-
-    record_timeout = args.record_timeout
-    phrase_timeout = args.phrase_timeout
-
-    temp_file = NamedTemporaryFile().name
-    transcription = ['']
 
     with source:
         recorder.adjust_for_ambient_noise(source)
+    recorder.listen_in_background(source, record_callback)
 
-    def record_callback(_, audio:sr.AudioData) -> None:
-        """
-        Threaded callback function to receive audio data when recordings finish.
-        audio: An AudioData containing the recorded bytes.
-        """
-        # Grab the raw bytes and push it into the thread safe queue.
-        data = audio.get_raw_data()
-        data_queue.put(data)
+    print("Now listening...")
 
-    # Create a background thread that will pass us raw audio bytes.
-    # We could do this manually but SpeechRecognizer provides a nice helper.
-    recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
-
-    # Cue the user that we're ready to go.
-    print("Model loaded.\n")
-
-    while True:
-        try:
-            now = datetime.utcnow()
-            # Pull raw recorded audio from the queue.
-            if not data_queue.empty():
-                phrase_complete = False
-                # If enough time has passed between recordings, consider the phrase complete.
-                # Clear the current working audio buffer to start over with the new data.
-                if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
-                    last_sample = bytes()
-                    phrase_complete = True
-                # This is the last time we received new audio data from the queue.
-                phrase_time = now
-
-                # Concatenate our current audio data with the latest audio data.
-                while not data_queue.empty():
-                    data = data_queue.get()
-                    last_sample += data
-
-                # Use AudioData to convert the raw data to wav data.
-                audio_data = sr.AudioData(last_sample, source.SAMPLE_RATE, source.SAMPLE_WIDTH)
-                wav_data = io.BytesIO(audio_data.get_wav_data())
-
-                # Write wav data to the temporary file as bytes.
-                with open(temp_file, 'w+b') as f:
-                    f.write(wav_data.read())
-
-                # Read the transcription.
-                result = audio_model.transcribe(temp_file, fp16=True)
-                text = result['text'].strip()
-
-                # If we detected a pause between recordings, add a new item to our transcription.
-                # Otherwise edit the existing one.
-                if phrase_complete:
-                    transcription.append(text)
-                else:
-                    transcription[-1] = text
-
-                # Clear the console to reprint the updated transcription.
-                os.system('cls' if os.name=='nt' else 'clear')
-                for line in transcription:
-                    print(line)
-                # Flush stdout.
-                print('', end='', flush=True)
-
-                # Infinite loops are bad for processors, must sleep.
-                sleep(0.25)
-        except KeyboardInterrupt:
-            break
-
-    print("\n\nTranscription:")
-    for line in transcription:
-        print(line)
+    # open a file with the current datetime as the name, without any special characters
+    with open(datetime.now().strftime("%Y_%m_%d-%H_%M_%S") + ".txt", "w") as outf:
+        while True:
+            try:
+                if not data_queue.empty():
+                    text = recorder.recognize_whisper(
+                        sr.AudioData(data_queue.get(), source.SAMPLE_RATE, source.SAMPLE_WIDTH), model=args.model
+                    ).strip()
+                    print(text)
+                    print(text, file=outf)
+                    sleep(0.25)
+            except KeyboardInterrupt:
+                break
 
 
 if __name__ == "__main__":
