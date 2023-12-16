d37361567adce9ed4c55700a0f65635142b296fe
Eric Hennenfent <ecapstone@gmail.com>
2023-12-16 03:16:06 -0800

    Add very rudimentary transitions
    they don't really work but w/e

--- live_illustrate/serve.py
+++ live_illustrate/serve.py
@@ -6,6 +6,8 @@ from flask import Flask, Response, send_from_directory
 log = logging.getLogger("werkzeug")
 log.setLevel(logging.ERROR)
 
+IMAGE_HTML = """<div hx-get="/image/{index}" hx-trigger="every 5s" hx-swap="outerHTML transition:true" class="imgbox"><img src='{image_url}' class='center-fit'/></div>"""
+
 
 class ImageServer:
     def __init__(self, host: str, port: int, default_image: str = "https://placehold.co/1792x1024/png") -> None:
@@ -17,13 +19,19 @@ class ImageServer:
         self.app = Flask(__name__)
 
         self.app.add_url_rule("/", "index", self.serve_index)
-        self.app.add_url_rule("/image", "image", self.serve_image_tag)
+        self.app.add_url_rule("/image/<index>", "image", self.serve_image_tag)
 
     def serve_index(self) -> Response:
         return send_from_directory("templates", "index.html")
 
-    def serve_image_tag(self) -> str:
-        return f"<img src='{self.images[-1]}' class='center-fit'/>"
+    def serve_image_tag(self, index: str) -> str:
+        if not index.isdigit():
+            index = -1
+        index = int(index)
+        if index not in range(len(self.images)):
+            index = -1
+        next_index = min(index + 1, len(self.images) - 1)
+        return IMAGE_HTML.format(index=next_index, image_url=self.images[next_index])
 
     def start(self) -> None:
         self.app.run(host=self.host, port=self.port)
